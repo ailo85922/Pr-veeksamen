6812b0ff9df6c46cd80d10110352110a56882711
ailo85922 <ainia002@osloskolen.no>
2026-05-06 10:28:37 +0200

    files added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ number = random.randint(1, 100)
 @app.route("/", methods=["GET", "POST"])
 def index():
     message = ""
+    won = False
 
     if request.method == "POST":
         guess = int(request.form["guess"])
@@ -17,6 +18,10 @@ def index():
         elif guess > number:
             message = "For høyt"
         else:
-            message = "Riktig!"
+            message = "Riktig! Du vant 🎉"
+            won = True
 
-    return render_template("index.html", message=message)
+    return render_template("index.html", message=message, won=won)
+
+if __name__ == "__main__":
+    app.run(debug=True)
