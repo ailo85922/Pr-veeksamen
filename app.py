b49962765fb7edc03c0136326b6044e0cc033cde
ailo85922 <ainia002@osloskolen.no>
2026-05-06 10:10:26 +0200

    first commit

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-number = 50  # fast tall (du kan senere gjøre det tilfeldig)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        guess = int(request.form["guess"])
-
-        if guess < number:
-            return "For lavt"
-        elif guess > number:
-            return "For høyt"
-        else:
-            return "Riktig!"
-
-    return '''
-        <form method="POST">
-            <input type="text" name="guess">
-            <button type="submit">Gjett</button>
-        </form>
-    '''
-
+if __name__ == "__main__":
+    app.run(debug=True)
