313c9ff9f285e2209f7a08b14e12da964e7a5f89
ailo85922 <ainia002@osloskolen.no>
2026-05-06 10:19:14 +0200

    files added

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 repository for prøveeksamen
 
 jeg startet med å søke opp hvordan jeg kombinerer repositoryen min med vs code, altså i github så jeg kan gjøre ulike kommandoer
+
+nå har jeg brukt litt KI til å få inn basicsa som jeg ikke husket, for eksempel noen kommandoer i terminalen, sånn at jeg kan oppdatere alt inn til git av det jeg gjør, også måtte jeg få en liten oppdatering på render template og hvilken funksjon det har.
+
+jeg har gått litt gjennom det man vanligvis burde kunne bare for å få en liten gjennomgang, også skal jeg se på de videoene om python og flask som erik har ute.
--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+number = random.randint(1, 100)
 
-@app.route("/")
-def home():
-    return 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    message = ""
+
+    if request.method == "POST":
+        guess = int(request.form["guess"])
+
+        if guess < number:
+            message = "For lavt"
+        elif guess > number:
+            message = "For høyt"
+        else:
+            message = "Riktig!"
+
+    return render_template("index.html", message=message)
